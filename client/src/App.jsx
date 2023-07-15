85135091c99efccc03a1bd5056d9f94668f18480
sor4chi <sorakawa0218@gmail.com>
2023-07-15 17:43:55 +0900

    feat(client): Loginしているかどうかを判断して、PostFormを出すか決める処理を作成

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { PostList } from "./components/PostList";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
 
   // APIから投稿データを取得する関数
   const getPosts = async () => {
@@ -18,16 +19,41 @@ function App() {
     setPosts(data);
   };
 
+  // ログインユーザーを取得する関数
+  const getAuthUser = async () => {
+    // ローカルストレージからトークンを取得
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    // APIからユーザー情報を取得
+    const res = await fetch(`${BACKEND_ENDPOINT}/api/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      // もし認証に成功すればユーザーデータが返ってくるので、それをセットする
+      setUser(data);
+    } else {
+      // 認証に失敗したら、トークンを削除する(有効期限切れや、偽のトークンが渡された場合など)
+      localStorage.removeItem("token");
+    }
+  };
+
   // useEffectを使って、このコンポーネントが描画された時に実行される処理を書く
   useEffect(() => {
     // APIから投稿データを取得
     getPosts();
+    // ログインユーザーを取得
+    getAuthUser();
   }, []);
 
   return (
     <main className="app-container">
       <h1>匿名掲示板（仮アプリ）</h1>
-      <PostForm addPost={(post) => setPosts([post, ...posts])} />
+      {/* ログイン済みの場合はPostFormを表示 */}
+      {user && <PostForm addPost={(post) => setPosts([post, ...posts])} />}
       <PostList posts={posts} />
     </main>
   );
